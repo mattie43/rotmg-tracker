6191ace5956dd02d936ef1e8e7306f9d355af9df
Matt Ericksen <mattericksen93@gmail.com>
2025-07-05 14:03:34 -0400

    reverse items order

--- src/app/updates/items.ts
+++ src/app/updates/items.ts
@@ -1,15 +1,16 @@
+// Put new items to top
 export const items = [
-  {
-    user: "Anonymous",
-    comment: "great work. would like staff member tracker like on realmstock's",
-    update: `Added a "Staff Member" filter to the events page side panel`,
-    timeStamp: 1751233921839,
-  },
   {
     user: "",
     comment: "",
     update:
       "When filtering events, your selected events are now put to the top of the list",
     timeStamp: 1751734060902,
+  },
+  {
+    user: "Anonymous",
+    comment: "great work. would like staff member tracker like on realmstock's",
+    update: `Added a "Staff Member" filter to the events page side panel`,
+    timeStamp: 1751233921839,
   },
 ];
